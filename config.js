d1f7a70e128ddb62f3824057de2fcbb76853f48e
Teja N <stycool24@gmail.com>
2015-06-08 15:09:19 -0400

    porting over code from Jeremey's repo with minor changes

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://zomdar:12201498@proximus.modulusmongo.net:27017/qUbeh4aj',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
